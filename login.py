1cb3f6acff0fe3617e6c00dff1fa5aef27a127b9
UAVFlightController <160624862+UAVFlightController@users.noreply.github.com>
2024-02-20 15:35:03 +0000

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+IST_ID = 'ist1102949'
+host = 'db.tecnico.ulisboa.pt'
+port = 5432
+password = 'yaec6386'
+db_name = IST_ID
+credentials = 'host={} port={} user={} password={} dbname={}'.format(host, port, IST_ID,
+password, db_name)
